bfcb7a10fd935ab30972ba3a6206a6897d796d0f
Samantha <samantha.sammy21@gmail.com>
2021-06-05 09:38:36 -0600

    Removed Confirm Password from backend

--- models/userModel.js
+++ models/userModel.js
@@ -2,13 +2,8 @@ const mongoose = require('mongoose')
 const {Schema, model} = mongoose
 
 const userSchema = new Schema({
-    username: {type: String, required: true},
-    password: {type: String, required: true},
-    confirmPassword: String,
-    userPosts: [{
-        type: Schema.Types.ObjectId,
-        ref: 'Post'
-    }]
+    username: {type: String, required: true, unique: true},
+    password: {type: String, required: true}
 })
 
 module.exports = model('User', userSchema)
