23dd77255ccff81efc0376e95197e6bfcd6e4767
Samantha <samantha.sammy21@gmail.com>
2021-06-05 09:12:39 -0600

    Session Controller corrected

--- controllers/session.js
+++ controllers/session.js
@@ -1,35 +1,48 @@
 const bcrypt = require('bcrypt')
 const express = require('express')
-const sessions = express.Router()
+const session = express.Router()
 const User = require('../models/userModel')
 
-// NEW Route -----
-sessions.get('/new', (req, res)=>{
-    res.send(console.log('New Session'))
-})
 
-// POST Login Route ---
+// GET Sessions Route ---
+session.get('/', (req, res)=>{
+    req.session.currentUser?
+    res.json({currentUser: req.session.currentUser}):
+    res.status(400).json({message: 'No current session active'})
+}) 
+
+
+
+// POST Create Session Route -----
 sessions.post('/', (req, res)=>{
-    User.findOne({ username: req.body.username}, (err, foundUser)=>{
+    userModel.findOne({ username: req.body.username}, (error, foundUsers)=>{
         if(error){
-            res.send(error)
-        } else if (!foundUser){
-            res.send(console.log('User Not Found'))
-        } else {
-            if(bcrypt.compareSync(req.body.password, foundUser.password)){
-                req.session.currentUser = foundUserres.send(console.log('User Found!'))
-            } else {
-                res.send(console.log("password doesn't match"))
+            res.status(400).json({message: error.message})
+        } 
+        else {
+            if(foundUsers){
+                if(bcrypt.compareSync(req.body.password, foundUsers.password)){
+                    req.session.currentUser = foundUsers
+                    res.status(200).json({message: 'Successfuly Logged In!' + foundUsers.username, currentUser: req.session.currentUsers})
+                } 
+                else {
+                    res.status(400).json({message: 'Incorrect Password / Incorrect Username!'})
+                } 
             }
         }
     })
 })
 
 // DELETE session Route ---
-sessions.delete('/', (req, res)=>{
-    req.session.destroy(()=>{
-        res.send(console.log('Session Ended'))
+session.delete('/', (req, res)=>{
+    req.session.destroy(error => {
+        if(error){
+            res.status(400).json({message: error.message})
+        }
+        else{
+            res.status(200).json({message: 'User is logged out'})
+        }
     })
 })
 
-module.exports = sessions
+module.exports = session
