f3c0dc5e1a7a7d8e5391b43249dc4417738cccc1
Samantha <samantha.sammy21@gmail.com>
2021-06-05 19:41:42 -0600

    commented out user model

--- models/userModel.js
+++ models/userModel.js
@@ -1,9 +1,9 @@
-const mongoose = require('mongoose')
-const {Schema, model} = mongoose
+// const mongoose = require('mongoose')
+// const {Schema, model} = mongoose
 
-const userSchema = new Schema({
-    username: {type: String, required: true, unique: true},
-    password: {type: String, required: true}
-})
+// const userSchema = new Schema({
+//     username: {type: String, required: true, unique: true},
+//     password: {type: String, required: true}
+// })
 
-module.exports = model('User', userSchema)
+// module.exports = model('User', userSchema)
