125db1cff40676868ad455f23c79d1f1628c2af8
Samantha <samantha.sammy21@gmail.com>
2021-06-05 19:38:13 -0600

    Commented out sessions and user login/register

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -1,41 +1,56 @@
 const express = require('express')
 const blog = express.Router()
 const postModel = require('../models/postModel')
-const userModel = require('../models/userModel')
+// const userModel = require('../models/userModel')
 
 // GET Route (Index of posts) -----
 blog.get('/', (req, res)=>{
     console.log('Index post working')
-    userModel.findById(req.session.currentUser._id).populate('blog').exec((error, foundUser)=>{
+    postModel.find({}, (error, foundPost)=> {
         if(error){
-            return res.status(400).json(error)
+            res.status(400).json(error)
         }
         else{
-            console.log(foundUser)
-            return res.status(200).json(foundUser.blog)
+            res.status(200).json(foundPost)
         }
     })
+    // userModel.findById(req.session.currentUser._id).populate('blog').exec((error, foundUser)=>{
+    //     if(error){
+    //         return res.status(400).json(error)
+    //     }
+    //     else{
+    //         console.log(foundUser)
+    //         return res.status(200).json(foundUser.blog)
+    //     }
+    // })
 })
 
 //POST create new Post review
-blog.post('/new', (req, res)=>{
-    console.log(req.session.currentUser)
+blog.post('/', (req, res)=>{
 
-    postModel.create(req.body, (error, createdPost)=>{
+    postModel.create(req.body, (error, createPost)=>{
         if(error){
             res.status(400).json({error: error.message})
-        }else{
-            userModel.findById(req.session.currentUser._id, (error, foundUser)=>{
-                if(error){
-                    res.status(400).json({error: error.message})
-                }else{
-                    foundUser.userPosts.push(createdPost)
-                    foundUser.save()
-                    res.status(201).json(createdPost)
-                }
-            })
+        }
+        else{
+            res.status(201).json(createPost)
         }
     })
+    // postModel.create(req.body, (error, createdPost)=>{
+    //     if(error){
+    //         res.status(400).json({error: error.message})
+    //     }else{
+    //         userModel.findById(req.session.currentUser._id, (error, foundUser)=>{
+    //             if(error){
+    //                 res.status(400).json({error: error.message})
+    //             }else{
+    //                 foundUser.userPosts.push(createdPost)
+    //                 foundUser.save()
+    //                 res.status(201).json(createdPost)
+    //             }
+    //         })
+    //     }
+    // })
 })
 
 
@@ -43,30 +58,51 @@ blog.post('/new', (req, res)=>{
 blog.put('/:id', (req, res)=>{
     console.log('PUT route working')
 
-    postsModel.findByIdAndUpdate(req.params.id, req.body, {new: true}, (error, updatedPost)=>{
+    postModel.findByIdAndUpdate(req.params.id, req.body, {new: true}, (error, updatePost)=>{
         if(error){
             res.status(400).json({error: error.message})
-        }else{
-            res.status(200).json({
-                message: `Entry ${updatedPost._id} Updated Successfully` ,
-                data: updatedPost
-            })
-            console.log('Update link was hit!', req.body)
+        }
+        else{
+            res.status(200).json({message: 'Updated post successfully', data: updatePost})
         }
     })
+
+    // postsModel.findByIdAndUpdate(req.params.id, req.body, {new: true}, (error, updatedPost)=>{
+    //     if(error){
+    //         res.status(400).json({error: error.message})
+    //     }else{
+    //         res.status(200).json({
+    //             message: `Entry ${updatedPost._id} Updated Successfully` ,
+    //             data: updatedPost
+    //         })
+    //         console.log('Update link was hit!', req.body)
+    //     }
+    // })
 })
 
 // DELETE Post Route ------
 blog.delete('/:id', (req, res)=>{
-    postsModel.findByIdAndDelete(req.params.id, (error, deletedPost)=>{
+
+    postModel.findByIdAndDelete(req.params.id, (error, deletedPost)=>{
         if(error){
             res.status(400).json({error: error.message})
-        }else if(deletedPost === null){
-            res.status(404).json({message: 'Entry not found'})
-        }else{
-            res.status(200).json({message: `Entry ${deletedPost.restName} Deleted Successfully`})
+        }
+        else if(deletedPost === null){
+            res.status(404).json({message: 'Post not found'})
+        }
+        else{
+            res.status(200).json({message: 'Deleted Successfully!'})
         }
     })
+    // postsModel.findByIdAndDelete(req.params.id, (error, deletedPost)=>{
+    //     if(error){
+    //         res.status(400).json({error: error.message})
+    //     }else if(deletedPost === null){
+    //         res.status(404).json({message: 'Entry not found'})
+    //     }else{
+    //         res.status(200).json({message: `Entry ${deletedPost.restName} Deleted Successfully`})
+    //     }
+    // })
 })
 
 module.exports = blog
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,50 +1,50 @@
-const bcrypt = require('bcrypt')
-const express = require('express')
-const users = express.Router()
-const userModel = require('../models/userModel')
+// const bcrypt = require('bcrypt')
+// const express = require('express')
+// const users = express.Router()
+// const userModel = require('../models/userModel')
 
 
-// POST Register Route/ NEW User Route ---
-users.post('/register', (req, res)=>{
-    req.body.password = bcrypt.hashSync(req.body.passwprd, bcrypt.genSaltSync(10))
+// // POST Register Route/ NEW User Route ---
+// users.post('/register', (req, res)=>{
+//     req.body.password = bcrypt.hashSync(req.body.passwprd, bcrypt.genSaltSync(10))
     
-    userModel.create(req.body, (error, newUser)=>{
-        if(error){
-            res.status(400).json({message: 'Username is already Taken', status: 400})
-        }
-        else{
-            res.status(200).json({message: `${newUser.username} created successfully!`, data: newUser})
-        }
-    })
-})
+//     userModel.create(req.body, (error, newUser)=>{
+//         if(error){
+//             res.status(400).json({message: 'Username is already Taken', status: 400})
+//         }
+//         else{
+//             res.status(200).json({message: `${newUser.username} created successfully!`, data: newUser})
+//         }
+//     })
+// })
 
-users.post('/login', (req, res)=> {
-    userModel.findOne({ username: req.body.username }, (error, foundUser)=>{
-        if(error){
-            res.send(error)
-        }
-        else{
-            if(foundUser){
-                if(bcrypt.compareSync(req.body.password, foundUser.password)){
-                    req.session.currentUser = foundUser 
-                    res.status(200).json(foundUser)
-                }
-                else{
-                    res.status(404).json({error: 'User not found!!'})
-                }
-            }
-            else{
-                res.status(400).json({error: error})
-            }
-        }
-    })
-})
+// users.post('/login', (req, res)=> {
+//     userModel.findOne({ username: req.body.username }, (error, foundUser)=>{
+//         if(error){
+//             res.send(error)
+//         }
+//         else{
+//             if(foundUser){
+//                 if(bcrypt.compareSync(req.body.password, foundUser.password)){
+//                     req.session.currentUser = foundUser 
+//                     res.status(200).json(foundUser)
+//                 }
+//                 else{
+//                     res.status(404).json({error: 'User not found!!'})
+//                 }
+//             }
+//             else{
+//                 res.status(400).json({error: error})
+//             }
+//         }
+//     })
+// })
 
 
-users.delete('/logout', (req, res)=>{
-    req.session.destroy(()=>{
-        res.status(200).json({message: 'Logged out successfully'})
-    })
-})
+// users.delete('/logout', (req, res)=>{
+//     req.session.destroy(()=>{
+//         res.status(200).json({message: 'Logged out successfully'})
+//     })
+// })
 
-module.exports = users
+// module.exports = users
